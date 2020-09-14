ec5e3f6824c37edf9ccdb6f99b3d1e3968b2ff1c
Nathan Stensvad <nstensvad@hotmail.com>
2020-09-14 16:59:27 -0700

    Add style

--- src/BookItem/BookItem.js
+++ src/BookItem/BookItem.js
@@ -5,14 +5,15 @@ class BookItem extends Component {
 
   render() {
     return (
-      <div>
+      <div id="book-item">
         <h2>{this.props.volumeInfo.title}</h2>
         <br />
         <div className="group">
-          <img className="item" src={this.props.volumeInfo.imageLinks.thumbnail} alt={this.props.volumeInfo.title}/>
-          <div className="item">
+          <img  src={this.props.volumeInfo.imageLinks.thumbnail} alt={this.props.volumeInfo.title}/>
+          <div>
             <p>Author: {(this.props.volumeInfo.hasOwnProperty('authors')) && this.props.volumeInfo.authors[0] }</p>
             <p>Price: ${((this.props.saleInfo.saleability === "FOR_SALE") || this.props.saleInfo.saleability === "FOR_SALE_AND_RENTAL") && this.props.saleInfo.listPrice.amount}</p>
+            <br/>
             <p>{this.props.volumeInfo.subtitle}</p>
           </div>
         </div>
--- src/Search/Search.js
+++ src/Search/Search.js
@@ -13,24 +13,27 @@ class Search extends Component {
     return (
       <div>
         <form onSubmit={this.formSubmitted}>
-          Search: <input type="text" name="bookName"/>
-          <input type="submit" value="Submit"/><br/>
-        
-          Print Type:
+          <div className="header-2">
+            Search: <input type="text" name="bookName" id="bookName" />
+            <input type="submit" value="Submit" /><br />
+          </div>
+          <div className="header-3">
+            Print Type:
         <select onChange={e => this.props.handlePrint(e.target.value)}>
-          <option value="all">All</option>
-          <option value="books">Books</option>
-          <option value="magazines">Magazines</option>
-        </select>
+              <option value="all">All</option>
+              <option value="books">Books</option>
+              <option value="magazines">Magazines</option>
+            </select>
           Book Type:
         <select onChange={e => this.props.handleBook(e.target.value)}>
-          <option value="">No Filter</option>
-          <option value="partial">Partial</option>
-          <option value="full">Full</option>
-          <option value="free-ebooks">Free Ebooks</option>
-          <option value="paid-ebooks">Paid Ebooks</option>
-          <option value="ebooks">Ebooks</option>
-        </select>
+              <option value="">No Filter</option>
+              <option value="partial">Partial</option>
+              <option value="full">Full</option>
+              <option value="free-ebooks">Free Ebooks</option>
+              <option value="paid-ebooks">Paid Ebooks</option>
+              <option value="ebooks">Ebooks</option>
+            </select>
+          </div>
         </form>
 
       </div>
